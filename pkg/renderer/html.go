a062e4492414146cad7385c8ea63f54cb0b2d5f6
Daniel Moreto <dfmoreto@gmail.com>
2025-07-29 10:19:44 -0300

    chore: adicionada documentação para rendering.HTML

--- pkg/renderer/html.go
+++ pkg/renderer/html.go
@@ -12,16 +12,22 @@ type templateConfig struct {
 	assetsMapping map[string]string
 }
 
+// HTML gerencia a renderização de páginas html
 var HTML templateConfig = templateConfig{
 	assetsMapping: map[string]string{},
 }
 
+// Configure sets values to be used over the HTML rendering
+// It receives the Asset URL path to be used when the page is rendered, the root views dir and the asset mapping to be parsed when some asset is provided
 func (tc *templateConfig) Configure(assetsUrlPath string, viewsDir string, assetsMapping map[string]string) {
 	tc.assetsUrlPath = assetsUrlPath
 	tc.viewsDir = viewsDir
 	tc.assetsMapping = assetsMapping
 }
 
+// Render builds an HTML with the sent data
+// It receives a view to rendered, that must be on Go Template format, along with the data to be include and returns an []byte and an error.
+// This function aims to encapsulate the HTML rendering implementation and provide functions to be used on the template file
 func (tc templateConfig) Render(view string, data any) ([]byte, error) {
 	baseFile := path.Base(view)
 	tmpl, err := template.New(baseFile).Funcs(template.FuncMap{
