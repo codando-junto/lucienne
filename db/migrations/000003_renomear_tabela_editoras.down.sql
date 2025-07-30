7f3571e714fc6b4657d315dc945613e9aff29c47
Gomex <rgomes@dnsfilter.com>
2025-07-30 20:05:42 -0300

    fix: ordem correta do down

--- db/migrations/000003_renomear_tabela_editoras.down.sql
+++ db/migrations/000003_renomear_tabela_editoras.down.sql
@@ -1,4 +1,4 @@
 ALTER TABLE publishers DROP CONSTRAINT publishers_name_key;
+ALTER TABLE publishers RENAME COLUMN name TO nome;
+ALTER TABLE publishers RENAME COLUMN id TO editora_id;
 ALTER TABLE publishers RENAME TO editoras;
-ALTER TABLE editoras RENAME COLUMN id TO editora_id;
-ALTER TABLE editoras RENAME COLUMN name TO nome;
