48d55ddbacc57d31f2afa70b2981d8117cb0ccb9
Gomex <rgomes@dnsfilter.com>
2025-07-30 20:04:10 -0300

    fix: precisamos garantir unicidade do campo name

--- db/migrations/000003_renomear_tabela_editoras.down.sql
+++ db/migrations/000003_renomear_tabela_editoras.down.sql
@@ -1,3 +1,4 @@
+ALTER TABLE publishers DROP CONSTRAINT publishers_name_key;
 ALTER TABLE publishers RENAME TO editoras;
 ALTER TABLE editoras RENAME COLUMN id TO editora_id;
 ALTER TABLE editoras RENAME COLUMN name TO nome;
