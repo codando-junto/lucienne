6c98c61c7071498d87ec084269389943f48bbc2b
JonasAlberto <137187041+BodeXX@users.noreply.github.com>
2025-08-13 22:45:26 -0300

    Update internal/handlers/main_test.go
    Co-authored-by: Luiz Aoqui <lgfa29@gmail.com>

--- internal/handlers/main_test.go
+++ internal/handlers/main_test.go
@@ -12,7 +12,7 @@ func TestMain(m *testing.M) {
 	// Configura as dependências necessárias para os handlers deste pacote antes de rodar os testes.
 	// Isso garante que os testes sejam autocontidos e não dependam da inicialização do pacote main.
 	config.Application.Configure("test")
-	viewsPath := path.Join(config.Application.RootPath, "internal/views")
+	viewsPath := path.Join(config.Application.RootPath, "internal", "views")
 	renderer.HTML.Configure("", viewsPath, nil)
 
 	// Roda todos os testes do pacote
