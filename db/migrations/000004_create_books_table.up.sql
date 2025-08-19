7a6e3574f0c9f22e60aea4ec049eb89df1d805ea
binlada25@gmail.com <binlada25@gmail.com>
2025-08-19 16:58:40 -0300

    fix(db): Corrige sintaxe na migração da tabela books

--- db/migrations/000004_create_books_table.up.sql
+++ db/migrations/000004_create_books_table.up.sql
@@ -1,8 +1,5 @@
 CREATE TABLE books (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    author_id INTEGER NOT NULL,
-    CONSTRAINT fk_author
-        FOREIGN KEY(author_id)
-        REFERENCES authors(id)
+    author_id INTEGER NOT NULL REFERENCES authors (id)
 );
